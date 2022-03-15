9b757e05ef9f4ce466f1811945606874048615db
Mike <mikz@itu.dk>
2022-03-15 09:24:46 +0100

    update deepsig

--- project/src/significance/check_significance.py
+++ project/src/significance/check_significance.py
@@ -3,7 +3,7 @@ import os
 
 import numpy as np
 import pandas as pd
-from deepsig import aso
+from deepsig import multi_aso
 
 
 # https://github.com/Kaleidophon/deep-significance/tree/main/deepsig
@@ -13,12 +13,12 @@ def check_significance(args: argparse.Namespace):
     num_comparisons = M * (M - 1) / 2
     eps_min = np.eye(M)  # M x M matrix with ones on diagonal
 
-    strategy_order = {"random"         : [],
+    strategy_order = {"random": [],
                       "leastconfidence": [],
-                      "entropy"        : [],
-                      "bald"           : [],
-                      "discriminative" : [],
-                      "cartography"    : []}
+                      "entropy": [],
+                      "bald": [],
+                      "discriminative": [],
+                      "cartography": []}
 
     for entry in os.scandir(f"{os.getenv('RESULTS_PATH')}{args.task}"):
         if entry.path.endswith(".csv") and not entry.path.endswith("analysis.csv"):
@@ -27,17 +27,10 @@ def check_significance(args: argparse.Namespace):
                 df = pd.read_csv(f, sep="\t")
                 scores = []
                 for row in df.itertuples():
-                    if row.step == 1500:
+                    if row.step == 1450:
                         scores.append(row.score)
                 strategy_order[strategy].extend(scores)
 
-    scores_a = list(strategy_order.values())
-    scores_b = scores_a
+    e_min = multi_aso(strategy_order, confidence_level=0.05, return_df=True, num_jobs=64)
 
-    for i in range(M):
-        for j in range(i + 1, M):
-            e_min = aso(scores_a[i], scores_b[j], confidence_level=0.05 / num_comparisons)
-            eps_min[i, j] = e_min
-            eps_min[j, i] = 1 - e_min
-
-    print(eps_min)
+    print(e_min)
