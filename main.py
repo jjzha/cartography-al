acc6e53c0055f786a7f2ce4efa027044c57b87e4
Mike <mikz@itu.dk>
2022-03-15 09:23:07 +0100

    add arguments for hyperparameters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import random
+import sys
 from collections import defaultdict
 
 import numpy as np
@@ -17,18 +18,36 @@ from project.src.utils.check_overlapping_indices import check_overlap_indices
 from project.src.utils.get_statistics_datamap import get_statistics_datamap
 from project.src.utils.save_indices_for_overlap import save_indices_for_overlap
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
 load_dotenv(verbose=True)
 
 
-def set_seed(seed: str) -> None:
-    torch.manual_seed(int(seed))
-    torch.cuda.manual_seed_all(int(seed))
+def setup_experiment(args, out_path):
+    if not os.path.exists(out_path):
+        # if output dir does not exist, create it (new experiment)
+        print(f"Path '{out_path}' does not exist. Creating...")
+        os.mkdir(out_path)
+    # if output dir exist, check if predicting
+    else:
+        print(f"Path '{out_path}' already exists. Exiting...")
+        exit(1)
+
+    # setup logging
+    log_format = '%(message)s'
+    log_level = logging.INFO
+    logging.basicConfig(filename=os.path.join(out_path, f'classify_{args.seed}.log'), filemode='a', format=log_format,
+                        level=log_level)
+    logger = logging.getLogger()
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def set_seed(seed: int) -> None:
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    np.random.seed(int(seed))
-    random.seed(int(seed))
-    os.environ["PYTHONHASHSEED"] = seed
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
 
 
 if __name__ == "__main__":
@@ -39,13 +58,22 @@ if __name__ == "__main__":
     parser.add_argument("--plot_results", action="store_true", help="Plots saved data.")
     parser.add_argument("--plot_dist_results", action="store_true", help="Plots saved data.")
     parser.add_argument("--check_indices", action="store_true", help="Checks selected indices.")
+    parser.add_argument("--exp_path", nargs="?", type=str, help="Path to experiment logs.")
+
     # Vectorization of data
     parser.add_argument("--vector", nargs="?", choices=["fasttext", "bow"])
     parser.add_argument("--pretrained", action="store_true", help="Uses pretrained embeddings.")
     parser.add_argument("--freeze", action="store_true", help="freezes pretrained embeddings.")
-    # Initial trainset size
+    # Hyperparameters
     parser.add_argument("--initial_size", nargs="?", type=int, help="Train size")
     parser.add_argument("--batch_size", nargs="?", type=int, help="batch_size")
+    parser.add_argument("--al_iterations", nargs="?", type=int, default=30, help="Active Learning Iterations.")
+    parser.add_argument("--seed", nargs="?", type=int, help="Seed to use.")
+    parser.add_argument("--epochs", nargs="?", type=int, default=50, help="Number of epochs to make the model run.")
+    parser.add_argument("--learning_rate_main", nargs="?", type=float, default=0.001, help="Learning rate for main "
+                                                                                           "classifier")
+    parser.add_argument("--learning_rate_binary", nargs="?", type=float, default=0.001, help="Learning rate for binary "
+                                                                                             "classifier")
     # Cartography specific arguments
     parser.add_argument("--cartography", action="store_true", help="Processes cartography.")
     parser.add_argument("--plot", action="store_true", help="Plots cartography.")
@@ -65,20 +93,21 @@ if __name__ == "__main__":
     elif args.significance:
         check_significance(args)
     else:
+        # setup experiment directory and logging
+        setup_experiment(args, args.exp_path)
         results, selected_top_k_runs = [], []
         confidence_run, variability_run, correctness_run = [], [], []
 
-        for seed in os.getenv("SEEDS").split():
-            set_seed(seed)
-            logging.info(f"Current seed: {seed} -- Active Learning process "
-                         f"with acquisition function: [{args.acquisition}]")
-            accuracy_history, selected_top_k, conf_stats, var_stats, corr_stats = start_active_learning(args)
+        set_seed(args.seed)
+        logging.info(f"Current args.seed: {args.seed} -- Active Learning process "
+                     f"with acquisition function: [{args.acquisition}]")
+        accuracy_history, selected_top_k, conf_stats, var_stats, corr_stats = start_active_learning(args)
 
-            results.append(accuracy_history)
-            selected_top_k_runs.append(selected_top_k)
-            confidence_run.append(conf_stats)
-            variability_run.append(var_stats)
-            correctness_run.append(corr_stats)
+        results.append(accuracy_history)
+        selected_top_k_runs.append(selected_top_k)
+        confidence_run.append(conf_stats)
+        variability_run.append(var_stats)
+        correctness_run.append(corr_stats)
 
         steps = int(os.getenv("ACTIVE_LEARNING_BATCHES"))
         total = steps * len(results[0])
@@ -90,6 +119,8 @@ if __name__ == "__main__":
             get_statistics_datamap(confidence_run, variability_run, correctness_run)
 
         df = pd.DataFrame({"score": [acc for val in results for acc in val],
-                           "step" : [step for _ in range(len(results)) for step in range(0, total, steps)]})
+                           "step": [step for _ in range(len(results)) for step in range(0, total, steps)]})
         logging.info(df)
-        df.to_csv(f"{os.getenv('RESULTS_PATH')}/{args.task}/{args.acquisition}_{args.initial_size}_{os.getenv('CAL_THRESHOLD')}_caldal.csv", sep="\t")
+        df.to_csv(
+            f"{os.getenv('RESULTS_PATH')}/{args.task}/{args.acquisition}_{args.initial_size}_{args.seed}.csv",
+            sep="\t")
