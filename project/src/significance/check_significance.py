41220d62a1e66cce184c612371936f0e938071c9
Mike <mikz@itu.dk>
2022-03-24 09:11:02 +0100

    add significance

--- project/src/significance/check_significance.py
+++ project/src/significance/check_significance.py
@@ -9,9 +9,6 @@ from deepsig import multi_aso
 # https://github.com/Kaleidophon/deep-significance/tree/main/deepsig
 
 def check_significance(args: argparse.Namespace):
-    M = 6  # Number of different models / algorithms
-    num_comparisons = M * (M - 1) / 2
-    eps_min = np.eye(M)  # M x M matrix with ones on diagonal
     strategies = ["bald", "cartography", "discriminative", "entropy", "leastconfidence", "random"]
 
     strategy_order = {"random": [],
@@ -24,19 +21,11 @@ def check_significance(args: argparse.Namespace):
     for strat in strategies:
         for entry in os.scandir(f"{os.getenv('RESULTS_PATH')}{args.task}"):
             strategy = entry.path.split("/")[-1].split("_")[0]
-<<<<<<< HEAD
             if entry.path.endswith(".csv") and strategy == strat:
                 with open(entry.path) as f:
                     df = pd.read_csv(f, sep="\t")
                     scores = []
                     for row in df.itertuples():
-=======
-            with open(entry.path) as f:
-                df = pd.read_csv(f, sep="\t")
-                scores = []
-                for row in df.itertuples():
-                    if row.step == 1450:
->>>>>>> ebd1d642135f9e01946c117f65afb47e7810230e
                         scores.append(row.score)
                     strategy_order[strategy].extend(scores)
 
